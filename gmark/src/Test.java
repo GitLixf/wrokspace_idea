25325ca621f998f98824a2bf061aef10e95a4022
lixf <lixf@163.com>
2021-02-22 23:47:19 +0800

    我的第一个idea工程

---
+++ gmark/src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * ClassName: Test
+ * Description:
+ * date: 2021/2/22 22:51
+ *
+ * @author 李学锋
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class Test {
+
+    public static void main(String[]args){
+        System.out.println("Hello World !!!");
+    }
+}
